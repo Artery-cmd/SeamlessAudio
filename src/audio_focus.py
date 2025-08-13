3d759d8c40fe613831f9b50eac817262e71d0184
Artery-cmd <140864642+Artery-cmd@users.noreply.github.com>
2025-08-13 19:54:08 +0100

    Update audio_focus.py

--- src/audio_focus.py
+++ src/audio_focus.py
@@ -1,14 +1,27 @@
 import subprocess
-import re
+from pydbus import SessionBus
 
-def pauseSpotify():
-  subprocess.run(["dbus-send", "--print -reply", "--dest=org.mpris.MediaPlayer2.spotify","/org/mpris/MediaPlayer2", "org.mpris.MediaPlayer2.Player.Pause"
+
+def pause_spotify(bus):
+  try: 
+    spotify = bus.get("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+    spotify.Pause()
+    print("[SeamlessAudio] Paused Spotify")
+  except Exception as e: 
+    #Spotify may not be running
+    print(f"[SeamlessAudio] Could not pause Spotify: {e}")
 
 #Listen to PulseAudio events
-proc = subprocess.Popen(["pactl", "subscribe"], stdout=subprocess.PIPE, text=True)
+def main():
+  bus = SessionBus()
+  proc = subprocess.Popen(["pactl", "subscribe"], stdout=subprocess.PIPE, text=True) #Holds list of processes
+  print("[SeamlessAudio] Listening for new audio streams...")
 
-for line in proc.stdout:
-  if "new" in line and "sink-input" in line:
-    sinkInputs = subprocess.check_output(["pactl", "list", "sink-inputs"], text=True
-    if "application.name = \"Spotify\"" in sinkInputs:
-      pauseSpotify()
+  for line in proc.stdout:
+    if "new" in line and "sink-input" in line:
+      sink_inputs = subprocess.check_output(["pactl", "list", "sink-inputs"], text=True)
+      if "application.name = \"Spotify\"" in sink_inputs:
+        pause_spotify(bus)
+
+if __name__ == "__main__":
+    main()
